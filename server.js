15aadf9d1025dcd3c59822c16a247e00888516c3
Mansour Ahmed <73757452+theEngMansour@users.noreply.github.com>
2022-10-02 13:20:40 +0300

    Subscription

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const next = require('next')
+const { createServer } = require('http')
 const { ApolloServer } = require('apollo-server-express')
 const { ApolloServerPluginDrainHttpServer } = require('apollo-server-core')
 const { makeExecutableSchema } = require('@graphql-tools/schema')
@@ -7,24 +8,11 @@ const { WebSocketServer } = require('ws')
 const { useServer } = require('graphql-ws/lib/use/ws')
 const { typeDefs } = require('./graphql/typeDefs')
 const { resolvers } = require('./graphql/resolvers')
-const { createServer } = require('http')
 
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
-
-app.prepare().then(() => {
-  const server = express()
-
-  server.all('*', (req, res) => {
-    return handle(req, res)
-  })
-
-  server.listen(port, () => {
-    console.log(`> Ready on http://localhost:${port}`)
-  })
-})
+const nextApp = next({ dev })
+const nextHandle = nextApp.getRequestHandler()
 
 async function startApolloServer(typeDefs, resolvers) {
   const app = express()
@@ -57,11 +45,15 @@ async function startApolloServer(typeDefs, resolvers) {
         }
     ]
   })
-  
+
+  nextApp.prepare().then(() => {
+    app.all('*', (req, res) => nextHandle(req, res))
+  })
+
   await servers.start()
   servers.applyMiddleware({ app })
-  await new Promise(resolve => httpServer.listen({ port: 4000 }, resolve))
-  console.log(`🚀 Server ready at http://localhost:4000${servers.graphqlPath}`)
+  await new Promise(resolve => httpServer.listen({ port }, resolve))
+  console.log(` Server ready at http://localhost:${port}${servers.graphqlPath}`)
 }
 
 startApolloServer(typeDefs, resolvers)
